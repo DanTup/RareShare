16f9cdfae3122d9594af4a1bf3579eccfbd3818a
Danny Tuppeny <danny@tuppeny.com>
2014-01-22 18:43:24 +0000

    RareCoordinator implementation
    Use generic SubscribeToChat method (future changeset) and old parsing
    logic copied from previous incarnation. Needs HealthPriority/Steps and
    Name fixing up!

---
+++ Publisher.RareCoordinator.lua
@@ -0,0 +1,27 @@
+﻿local function handleMessage(sender, rcMessage)
+	local parts = rcMessage:split("[^_]+")
+	if #parts ~= 4 then return end
+
+	local rare = { Zone = "Timeless Isle", SourceCharacter = sender, SourcePublisher = "RareCoordinator", Name = "FAKENAME-TODO" }
+	rare.ID = tonumber(parts[2])
+
+	local rcEventType = parts[3]
+	if rcEventType == "dead" then
+		rare.EventType = "Dead"
+	end
+	if rcEventType:starts("alive") then
+		rare.EventType = "Alive"
+		local loc = rcEventType:split("[^,%-]+")
+		if #loc == 3 then
+			rare.X = tonumber(loc[2])
+			rare.Y = tonumber(loc[3])
+		end
+		
+		rare.Health = tonumber(string.sub(loc[1], 6))
+		-- TODO: Set health priority (this goes down only in 10s)
+	end
+
+	RareShare:Publish(rare)
+end
+
+RareShare:SubscribeToChat("RCELVA", handleMessage)
