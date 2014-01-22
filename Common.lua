bf2ce80caf775a27bf86c59bbde308f25810c337
Danny Tuppeny <danny@tuppeny.com>
2014-01-22 18:41:46 +0000

    Add string split function

--- Common.lua
+++ Common.lua
@@ -1,3 +1,14 @@
 ﻿function string:starts(start)
 	return self:sub(1, start:len()) == start
 end
+
+function string:split(pattern)
+	pattern = pattern or "[^%s]+"
+	if pattern:len() == 0 then pattern = "[^%s]+" end
+	local parts = {__index = table.insert}
+	setmetatable(parts, parts)
+	self:gsub(pattern, parts)
+	setmetatable(parts, nil)
+	parts.__index = nil
+	return parts
+end
