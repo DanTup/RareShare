0ba7d0e5797b263f09bf78b908682e1fa15cdf1b
Danny Tuppeny <danny@tuppeny.com>
2014-01-22 17:27:23 +0000

    Update DebugChat subscriber with global/zone
    Coloured text, and show differences between your-zone alerts from all
    alerts to aid debugging

--- Subscriber.DebugChat.lua
+++ Subscriber.DebugChat.lua
@@ -1,14 +1,23 @@
-﻿local function DumpRareToChat(rare)
+﻿local function DumpRareToChat(rare, prefix)
 	local flags = ""
 	if rare.SuppressAnnouncements then flags = flags.." (noannounce)" end
 	if rare.MajorEvent then flags = flags.." (major)" end
 	if rare.EventType == "Alive" then
-		print("RareShare Debug: ("..rare.SourceCharacter.." @ "..rare.SourcePublisher..") "..rare.ID.." ("..rare.Name..") alive ("..rare.Health.."% HP) at ("..rare.X..","..rare.Y..") in "..rare.Zone..flags)
+		print("|cff9999ffRareShare Debug:|r "..prefix.."|cff999999 ("..rare.SourceCharacter.." @ "..rare.SourcePublisher..") "..rare.ID.." ("..rare.Name..") alive ("..rare.Health.."% HP) at ("..rare.X..","..rare.Y..") in "..rare.Zone..flags)
 	elseif rare.EventType == "Dead" then
-		print("RareShare Debug: ("..rare.SourceCharacter.." @ "..rare.SourcePublisher..") "..rare.ID.." dead in "..rare.Zone..flags)
+		print("|cff9999ffRareShare Debug:|r "..prefix.."|cff999999 ("..rare.SourceCharacter.." @ "..rare.SourcePublisher..") "..rare.ID.." dead in "..rare.Zone..flags)
 	end
 end
 
+local function DumpRareToChatUnfiltered(rare)
+	DumpRareToChat(rare, "|cffffff99GLOBAL|r")
+end
+
+local function DumpRareToChatFiltered(rare)
+	DumpRareToChat(rare, "|cffff9999ZONE|r")
+end
+
 if RareShare:IsDebugMode() then
-	RareShare:RegisterSubscriber(DumpRareToChat)
+	RareShare:RegisterSubscriber(DumpRareToChatUnfiltered, true)
+	RareShare:RegisterSubscriber(DumpRareToChatFiltered)
 end
