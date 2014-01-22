14f0f73d8bcea3de983c847a6bb8b5d17b467d4c
Danny Tuppeny <danny@tuppeny.com>
2014-01-22 18:44:12 +0000

    Generic SubscribeToChat functionality
    Plus better validation error messages

--- RareShare.lua
+++ RareShare.lua
@@ -17,32 +17,33 @@
 RareShare = {}
 local filteredSubscribers = {}
 local unfilteredSubscribers = {}
-local knownrares = {}
+local knownRares = {}
+local chatSubscribers = {} -- NOTE: This doesn't get reset, as registered handlers are added at runtime; it's all setup stuff
 
 -- Used for testing; need to be able to reset everything back to clean so that
 -- each test gets a clean run
 function RareShare:ResetState()
 	filteredSubscribers = {}
 	unfilteredSubscribers = {}
-	knownrares = {}
+	knownRares = {}
 end
 
 function RareShare:IsDebugMode() return true end
 
 function RareShare:ValidateRare(rare)
-	if rare == nil then return false end
-	if rare.SourceCharacter == nil then return false end
-	if rare.SourcePublisher == nil then return false end
-	if rare.ID == nil then return false end
-	if rare.Zone == nil then return false end
-	if rare.EventType ~= "Alive" and rare.EventType ~= "Dead" then return false end
+	if rare == nil then return "rare == nil" end
+	if rare.SourceCharacter == nil then return "SourceCharacter" end
+	if rare.SourcePublisher == nil then return "SourcePublisher" end
+	if rare.ID == nil then return "ID" end
+	if rare.Zone == nil then return "Zone" end
+	if rare.EventType ~= "Alive" and rare.EventType ~= "Dead" then return "EventType" end
 	if rare.EventType == "Alive" then
-		if rare.Name == nil then return false end
-		if rare.Health == nil then return false end
-		if rare.X == nil then return false end
-		if rare.Y == nil then return false end
+		if rare.Name == nil then return "Name" end
+		if rare.Health == nil then return "Health" end
+		if rare.X == nil then return "X" end
+		if rare.Y == nil then return "Y" end
 	end
-	return true
+	return nil
 end
 
 function RareShare:RegisterSubscriber(sub, includeOutOfZoneEvents)
@@ -54,19 +55,20 @@ function RareShare:RegisterSubscriber(sub, includeOutOfZoneEvents)
 end
 
 function RareShare:Publish(rare)
-	if not RareShare:ValidateRare(rare) then
-		--if RareShare:IsDebugMode() then print("Invalid rare! "..debug.traceback()) end
+	local validationResults = RareShare:ValidateRare(rare)
+	if validationResults ~= nil then
+		--if RareShare:IsDebugMode() then print("    Invalid rare! "..validationResults) end
 		return
 	end
 	
 	-- Set as Major event if it's a death, or alive but we didn't already have it
-	rare.MajorEvent = (rare.EventType == "Dead" or (rare.EventType == "Alive" and not knownrares[rare.ID]))
+	rare.MajorEvent = (rare.EventType == "Dead" or (rare.EventType == "Alive" and not knownRares[rare.ID]))
 
 	-- Keep track of the rare if it's alive, otherwise remove it from our tracking list
 	if rare.EventType == "Alive" then
-		knownrares[rare.ID] = rare
+		knownRares[rare.ID] = rare
 	else
-		knownrares[rare.ID] = nil
+		knownRares[rare.ID] = nil
 	end
 
 	-- Always notify subscribers that wanted unfiltered events
@@ -80,4 +82,42 @@ function RareShare:Publish(rare)
 			sub(rare)
 		end
 	end
+end
+
+function RareShare:SubscribeToChat(chatChannel, handler)
+	local function onChat(message, sender, language, channelString, target, flags, unknown, channelNumber, channelName, unknown, counter, guid)
+		if channelName == chatChannel then
+			handler(sender, message)
+		end
+	end
+
+	local function onEvent(self, event, ...)
+		if event == "CHAT_MSG_CHANNEL" then
+			onChat(...)
+		end
+	end
+
+	-- HACK: We need to wait some time for the default channels to be joined, otherwise we push General down from 1, and things go wonky
+	local timeTillJoinChannels = 5.0
+	local function onUpdate(self, elapsed)
+		timeTillJoinChannels = timeTillJoinChannels- elapsed
+
+		if timeTillJoinChannels < 0 then
+			JoinTemporaryChannel(chatChannel, nil, frame:GetID())
+			rareShareRCFrame:SetScript("OnUpdate", nil)
+		end
+	end
+
+	local frame = CreateFrame("MessageFrame", "RareShareRC")
+	frame:SetScript("OnEvent", onEvent)
+	frame:SetScript("OnUpdate", onUpdate)
+	frame:RegisterEvent("CHAT_MSG_CHANNEL")
+
+	local channelSubscribers = chatSubscribers[chatChannel] or {}
+	channelSubscribers[#channelSubscribers + 1] = handler
+	chatSubscribers[chatChannel] = channelSubscribers
+end
+
+function RareShare:GetChannelSubscribersForTesting()
+	return chatSubscribers
 end
