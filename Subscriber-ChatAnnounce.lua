790ad14ca5f0e7841923345f4c185df69ce2244c
Danny Tuppeny <danny@tuppeny.com>
2014-01-23 12:34:14 +0000

    Nicer formatting of chat announcements

--- Subscriber-ChatAnnounce.lua
+++ Subscriber-ChatAnnounce.lua
@@ -1,9 +1,9 @@
 ﻿local function AnnounceInChat(rare)
 	if rare.MajorEvent then
 		if rare.EventType == "Alive" then
-			SendChatMessage("(RareShare) "..rare.Name.." spotted around "..rare.X..","..rare.Y.." with "..rare.Health.." HP!", "GUILD")
+			SendChatMessage("[RareShare] "..rare.Name.." spotted around "..rare.X..","..rare.Y.." with "..rare.Health.." HP!", "GUILD")
 		elseif rare.EventType == "Dead" then
-			SendChatMessage("(RareShare) "..rare.Name.." has been killed!", "GUILD")
+			SendChatMessage("[RareShare] "..rare.Name.." has been killed!", "GUILD")
 		end
 	end
 end
