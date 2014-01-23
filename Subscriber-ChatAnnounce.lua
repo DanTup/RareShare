9385553a182feb4c4494ebbf9a7b912046edab05
Danny Tuppeny <danny@tuppeny.com>
2014-01-23 18:13:19 +0000

    Only announce deaths if you announced alive, to reduce CombatLog Death spam

--- Subscriber-ChatAnnounce.lua
+++ Subscriber-ChatAnnounce.lua
@@ -1,9 +1,16 @@
-﻿local function AnnounceInChat(rare)
+﻿-- Track which rares we announced, so we only announce deaths if we announced alives
+-- This is to avoid lots of Dead announcements from multiple RareShare users that
+-- were within range of the combat log announcement
+local announcedRares = {}
+
+local function AnnounceInChat(rare)
 	if rare.MajorEvent and rare.AllowAnnouncing then
 		if rare.EventType == "Alive" then
 			SendChatMessage("[RareShare] "..rare.Name.." spotted around "..rare.X..","..rare.Y.." with "..rare.Health.."% HP!", "GUILD")
-		elseif rare.EventType == "Dead" then
+			announcedRares[rare.ID] = true
+		elseif rare.EventType == "Dead" and announcedRares[rare.ID] then
 			SendChatMessage("[RareShare] "..rare.Name.." has been killed!", "GUILD")
+			announcedRares[rare.ID] = nil
 		end
 	end
 end
