66ab7af2ba6b3d8ec43eb8c5d65be53fa1692a96
Danny Tuppeny <danny@tuppeny.com>
2014-01-23 13:45:34 +0000

    Implementation of CombatLog parsing for Death events

---
+++ Publisher-CombatLogDeaths.lua
@@ -0,0 +1,26 @@
+﻿function handleCombatLogDeaths(timeStamp, event, hideCaster, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags)
+	if event ~= "UNIT_DIED" then return end
+
+	local rareID = UnitIDFromGuid(destGUID)
+
+	local rare = {
+		ID = rareID,
+		Name = destName,
+		Zone = GetZoneText(),
+		EventType = "Dead",
+		SourceCharacter = UnitName("player"),
+		SourcePublisher = "RareShareCombatLog"
+	}
+
+	RareShare:Publish(rare)
+end
+
+function onEvent(self, event, ...)
+	if event == "COMBAT_LOG_EVENT_UNFILTERED" then
+		handleCombatLogDeaths(...)
+	end
+end
+
+local frame = CreateFrame("MessageFrame", "RareShareCombatLogDeaths")
+frame:SetScript("OnEvent", onEvent)
+frame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
