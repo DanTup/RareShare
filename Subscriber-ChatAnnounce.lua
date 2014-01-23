e5ffec36ed4883463eb924d22e6feb1e3f5b612f
Danny Tuppeny <danny@tuppeny.com>
2014-01-23 19:15:57 +0000

    Fix general chat ;-)

--- Subscriber-ChatAnnounce.lua
+++ Subscriber-ChatAnnounce.lua
@@ -3,9 +3,18 @@
 -- were within range of the combat log announcement
 local announcedRares = {}
 
+local function getGeneralChat(id, name, ...)
+   if id and name then
+	  if strfind(name, GENERAL) then
+		 return id
+	  end
+	  return FindGeneralChat(...)
+   end
+end
+
 local function AnnounceInChat(rare)
-	local channelNumber, _ = GetChannelName("General")
-	if channelNumber == 0 then return end
+	local channelNumber = getGeneralChat(GetChannelList())
+	if channelNumber == 0 or channelNumber == nil then return end
 
 	if rare.MajorEvent and rare.AllowAnnouncing then
 		if rare.EventType == "Alive" then
