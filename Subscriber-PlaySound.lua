e87d7f757fe095a45711a50eff33cc4301e42b7c
Danny Tuppeny <danny@tuppeny.com>
2014-01-23 12:13:35 +0000

    Added subscriber to play sounds on major alive events

---
+++ Subscriber-PlaySound.lua
@@ -0,0 +1,7 @@
+﻿local function PlaySound(rare)
+	if rare.EventType == "Alive" and rare.MajorEvent then
+		PlaySoundFile("sound\\CREATURE\\MANDOKIR\\VO_ZG2_MANDOKIR_LEVELUP_EVENT_01.ogg", "master")		
+	end
+end
+
+RareShare:RegisterSubscriber(PlaySound)
