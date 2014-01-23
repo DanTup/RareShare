f7dd96e4e4ae1a3adb874171dfbf7cd226794baa
Danny Tuppeny <danny@tuppeny.com>
2014-01-23 14:12:22 +0000

    Implemented TomTom support

---
+++ Subscriber-TomTom.lua
@@ -0,0 +1,23 @@
+﻿local waypoints = {}
+
+local function UpdateWaypoints(rare)
+	if TomTom == nil then return end
+
+	-- Remove any existing waypoint
+	local existingPoint = waypoints[rare.ID]
+	if existingPoint ~= nil then
+		TomTom:RemoveWaypoint(existingPoint)
+		waypoints[rare.ID] = nil
+	end
+
+	-- If the event came from the current character, don't create a Waypoint; they clearly know where it is!
+	if rare.SourceCharacter == UnitName("player") then return end
+
+	if rare.EventType == "Alive" then
+		local waypointName = rare.Name.." ("..rare.Health.."% HP)    [RareShare]"
+		local newPoint = TomTom:AddWaypoint(rare.X, rare.Y, waypointName)
+		waypoints[rare.ID] = newPoint
+	end
+end
+
+RareShare:RegisterSubscriber(UpdateWaypoints)
