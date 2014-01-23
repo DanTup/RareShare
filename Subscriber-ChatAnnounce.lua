a5e96805d62119a618591509cbac96a22a37d6a1
Danny Tuppeny <danny@tuppeny.com>
2014-01-23 12:27:00 +0000

    Announce in chat on major events
    Currently set to Guild while testing in-game

---
+++ Subscriber-ChatAnnounce.lua
@@ -0,0 +1,11 @@
+﻿local function AnnounceInChat(rare)
+	if rare.MajorEvent then
+		if rare.EventType == "Alive" then
+			SendChatMessage("(RareShare) "..rare.Name.." spotted around "..rare.X..","..rare.Y.." with "..rare.Health.." HP!", "GUILD")
+		elseif rare.EventType == "Dead" then
+			SendChatMessage("(RareShare) "..rare.Name.." has been killed!", "GUILD")
+		end
+	end
+end
+
+RareShare:RegisterSubscriber(AnnounceInChat)
